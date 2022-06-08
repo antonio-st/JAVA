0d6ef771676de48a1c7041cd1f53d867118729ec
#Anton <90837472+antonio-st@users.noreply.github.com>
2022-06-08 12:10:14 +0300

    Update Main.java
    add comment

--- Lesson_netology/NET_less1/src/Main.java
+++ Lesson_netology/NET_less1/src/Main.java
@@ -10,41 +10,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Угадай слово! ");
-        int lengthWord = word.length();
-        String maskWord = "-".repeat(lengthWord);
-        System.out.println(maskWord);
+        int lengthWord = word.length(); // ищем длину слова javalove
+        String maskWord = "-".repeat(lengthWord); // повторяем "-" по кол-ву равным длине слова lengthWord и записываем в переменную 
+        System.out.println(maskWord); // выводим символы
 
         do {
-            System.out.println("Введите букву");
-            char c = input.next().charAt(0);
+            System.out.println("Введите букву"); // просим пользователя ввести букву
+            char c = input.next().charAt(0); // проверяем первый символ, записываем в переменную c     
+       
 
-            if (word.indexOf(c) >= 0) {
+            if (word.indexOf(c) >= 0) {      // a -> 2 , если w -> -1
                 System.out.println("Удача, угадал!");
-                for (char elem : word.toCharArray()) {
-                    if (elem == c) {
-                        maskWord = replaceMaskLetter(c, maskWord);
+                for (char elem : word.toCharArray()) { // разбираем оригинальное слово на список элементов
+                    if (elem == c) { //если введенный символ совпадает с оригинальным словом
+                        maskWord = replaceMaskLetter(c, maskWord); //если это так , открываем букву (см. метод)
                     }
                 }
-                System.out.println(maskWord);
+                System.out.println(maskWord); //печатаем
             } else {
                 System.out.println("Промах, попробуй еще раз!");
-                System.out.println(maskWord);
+                System.out.println(maskWord); //выводим то , что уже отгадали
             }
-        } while (maskWord.contains("-"));
+        } while (maskWord.contains("-")); //выполняется пока в маскированном слове есть "-"
         System.out.println("Поздравляем, ты выиграл!");
     }
 
-    public static String replaceMaskLetter(char c, String maskWord) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == c) {
+    public static String replaceMaskLetter(char c, String maskWord) { //метод, на вход получает символ и маскированное слово
+        StringBuilder stringBuilder = new StringBuilder(); //заводим переменную для внесения данных с маски слова
+        for (int i = 0; i < word.length(); i++) { // в цикле проходим по слову 
+            if (word.charAt(i) == c) { //если буква совпадает, то добавляем ее
                 stringBuilder.append(c);
-            } else if (maskWord.charAt(i) != '-') {
+            } else if (maskWord.charAt(i) != '-') { //добавляем те элементы, которые уже открыты
                 stringBuilder.append(maskWord.charAt(i));
             } else {
-                stringBuilder.append("-");
+                stringBuilder.append("-"); // если это не отгаданный и не открытый символ, добавляем "-"
             }
         }
-        return stringBuilder.toString();
+        return stringBuilder.toString(); //возвращаем значения
     }
 }
